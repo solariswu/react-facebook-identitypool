833ce8a6800cb231984ac15cf0dbb5ccc257b9a7
Yungang <yunganw@amazon.com>
2021-07-29 22:32:10 +1000

    change back

--- src/SignInWithFb.js
+++ src/SignInWithFb.js
@@ -113,7 +113,7 @@ const SignInWithFacebook = () => {
     // init the fb sdk client
     const fb = window.FB;
     fb.init({
-      appId: "2872561679644543",
+      appId: "182998633881578",
       cookie: true,
       xfbml: true,
       version: "v2.11",
